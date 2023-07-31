108c5e973b80363c6745b83dcdb1931c875b4184
Carlos Urdaneta <curdanetp@users.noreply.github.com>
2023-07-30 20:14:08 -0400

    Ajustes del Eks Cluster Version

--- 02-eks/output.tf
+++ 02-eks/output.tf
@@ -22,22 +22,14 @@ output "eks_cluster_id" {
 }
 
 output "eks_cluster_version" {
-  value = module.eks.cluster_platform_version
+  value = module.eks.cluster_version
 }
 
-/*
-output "aws_eks_cluster_auth_token" {
-  value = data.aws_eks_cluster_auth.cluster.token
-  sensitive = true
+
+//---------------
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = module.eks.cluster_name
 }
-output "aws_eks_cluster_certificate_authority_data" {
-  value = data.aws_eks_cluster.cluster.certificate_authority.0.data
-  sensitive = true
-}
-output "aws_eks_cluster_endpoint" {
-  value = data.aws_eks_cluster.cluster.endpoint
-}
-*/
 #--------------------------------------------------------- Cloudwatch
 output "cloudwatch_log_group_arn" {
   value = module.eks.cloudwatch_log_group_arn
--- 03-argocd/providers.tf
+++ 03-argocd/providers.tf
@@ -1,25 +1,29 @@
+data "aws_eks_cluster_id" "default" {
+  name = data.terraform_remote_state.eks.outputs.eks_cluster_id
+}
+data "aws_eks_cluster_auth" "default" {
+  name = data.terraform_remote_state.eks.outputs.eks_cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster_auth" {
+  name = data.terraform_remote_state.eks.outputs.cluster_name
+}
+
+
 provider "aws" {
   region = "us-east-1"
 }
 
-data "aws_eks_cluster" "this" {
-  name = data.terraform_remote_state.eks.outputs.eks_cluster_id
-}
-
-data "aws_eks_cluster_auth" "this" {    
-  name = data.terraform_remote_state.eks.outputs.eks_cluster_id
-}
-
 provider "kubernetes" {
-  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-  token                  = data.terraform_remote_state.eks.outputs.aws_eks_cluster_auth_token
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.aws_eks_cluster_certificate_authority_data)
+  host                   = data.aws_eks_cluster.default.endpoint
+  token                  = data.aws_eks_cluster_auth.default.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
 }
 
 provider "helm" {
   kubernetes {
-  host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
-  token                  = data.terraform_remote_state.eks.outputs.aws_eks_cluster_auth_token
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.aws_eks_cluster_certificate_authority_data)
+    host                   = data.terraform_remote_state.eks.outputs.cluster_endpoint
+    token                  = "${data.aws_eks_cluster_auth.cluster_auth.token}"
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
   }
 }
